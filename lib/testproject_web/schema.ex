9991f47954dbcb3d8f162f3bf9498fc2d3d48e9b
rai-kargo <adwitya.paramartha@kargo.tech>
2022-06-02 11:52:14 +0700

    Graph QL, Get & Create Transporter Mutation

--- lib/testproject_web/schema.ex
+++ lib/testproject_web/schema.ex
@@ -12,10 +12,26 @@ defmodule TestprojectWeb.Schema do
         {:ok, Testproject.Vehicles.get_vehicle!(id)}
       end)
     end
+
+    field :get_transporter_by_id, :transporter do
+      arg(:id, non_null(:id))
+      resolve(fn  _entity, %{id: id}, _context->
+        {:ok, Testproject.Transporters.get_transporter!(id)}
+      end)
+    end
   end
-  # mutation do
-  # # this is the mutation entry point to our app
-  # end
+
+
+  mutation do
+  # this is the mutation entry point to our app
+  field :create_transporter, :transporter do
+    arg(:input, :transporter_input )
+    resolve(fn  _entity, %{input: transporter_params}, _context->
+       Testproject.Transporters.create_transporter(transporter_params)
+    end)
+  end
+
+  end
 
 
   object :vehicle do
@@ -23,6 +39,23 @@ defmodule TestprojectWeb.Schema do
     field :license_plate, :string
     field :status, :string
     end
+
+    object :transporter do
+      field :id, :integer
+      field  :name, :string
+      field :phone_number, :string
+      field :npwp_number, :string
+      field :status, :string
+      end
+
+      input_object :transporter_input do
+        field  :id, :id
+        field  :name, :string
+        field :phone_number, :string
+        field :npwp_number, :string
+        field :status, :string
+        end
+
 
 
  end
